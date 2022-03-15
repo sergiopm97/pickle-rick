0f2ff8f16955d12d50dba8f33e7f57a0d44bcbca
Sergio Peña Muñoz <92332731+sergiopm97@users.noreply.github.com>
2022-03-15 20:17:21 +0100

    Create module and component header
    Creation of the module and the component header and implementation of the same within the home block

--- src/public/home/home.module.ts
+++ src/public/home/home.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { HeaderModule } from 'src/shared/components/header/header.module';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, HeaderModule],
   exports: [HomeComponent],
 })
 export class HomeModule {}
