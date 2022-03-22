a5f062bc2ac05a84e15b5faff59a9c5bdb17c9fa
Sergio Peña Muñoz <92332731+sergiopm97@users.noreply.github.com>
2022-03-22 19:38:38 +0100

    Implement welcome block
    Creation and implementation of the module and the welcome component in their original and responsive versions

--- src/public/home/home.module.ts
+++ src/public/home/home.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HeaderModule } from 'src/shared/components/header/header.module';
+import { WelcomeModule } from './welcome/welcome.module';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HeaderModule],
+  imports: [CommonModule, HeaderModule, WelcomeModule],
   exports: [HomeComponent],
 })
 export class HomeModule {}
