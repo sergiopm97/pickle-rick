de2b0b7181a54b2796f1253fcc7410736a285114
Sergio Peña Muñoz <92332731+sergiopm97@users.noreply.github.com>
2022-03-24 20:11:29 +0100

    Implement explanation block
    Creation and implementation of the module and the explanation component in their original and responsive versions

--- src/public/home/home.module.ts
+++ src/public/home/home.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HeaderModule } from 'src/shared/components/header/header.module';
 import { WelcomeModule } from './welcome/welcome.module';
+import { ExplanationModule } from './explanation/explanation.module';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HeaderModule, WelcomeModule],
+  imports: [CommonModule, HeaderModule, WelcomeModule, ExplanationModule],
   exports: [HomeComponent],
 })
 export class HomeModule {}
