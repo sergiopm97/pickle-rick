0d2c98ced8f4f9255afcd25adb28b6ea282a90ce
sergiopm97 <92332731+sergiopm97@users.noreply.github.com>
2022-05-07 19:21:01 +0200

    Dynamize character template
    Generation of the character interface and dynamization of its variables within its own template

--- src/public/characters/character/character.component.ts
+++ src/public/characters/character/character.component.ts
@@ -1,12 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ajax } from 'rxjs/ajax';
 
 @Component({
   selector: 'app-character',
   templateUrl: './character.component.html',
   styleUrls: ['./character.component.scss'],
 })
+
 export class CharacterComponent implements OnInit {
-  constructor() {}
+  id: string = String();
+  character = new Array<any>();
+  constructor(private route: ActivatedRoute) {
+    interface CharacterFull {
+      id: number,
+      name: string,
+      status: string,
+      species: string,
+      gender: string,
+      origin: string,
+      location: string,
+      image: string,
+      episode: Array<string>,
+    }
+    this.route.params.subscribe((params) => {
+      this.id = params['id'].toString();
+    });
+    ajax(`https://rickandmortyapi.com/api/character/${this.id}`).subscribe(
+      (res: any) => {
+        let characterObject: CharacterFull = {
+          id: res.response["id"],
+          name: res.response["name"],
+          status: res.response["status"],
+          species: res.response["species"],
+          gender: res.response["gender"],
+          origin: res.response["origin"],
+          location: res.response["location"],
+          image: res.response["image"],
+          episode: res.response["episode"]
+        }
+        this.character.push(characterObject);
+      }
+    );
+  }
 
   ngOnInit(): void {}
 }
