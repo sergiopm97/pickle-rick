cc779f263007b4293801e4cf521b02b552c4d81e
Sergio Peña Muñoz <92332731+sergiopm97@users.noreply.github.com>
2022-05-08 18:03:30 +0200

    Create character page
    Creation of the user interface of the page displaying the details of a particular character

--- src/public/characters/character/character.component.ts
+++ src/public/characters/character/character.component.ts
@@ -7,21 +7,17 @@ import { ajax } from 'rxjs/ajax';
   templateUrl: './character.component.html',
   styleUrls: ['./character.component.scss'],
 })
-
 export class CharacterComponent implements OnInit {
   id: string = String();
   character = new Array<any>();
   constructor(private route: ActivatedRoute) {
     interface CharacterFull {
-      id: number,
-      name: string,
-      status: string,
-      species: string,
-      gender: string,
-      origin: string,
-      location: string,
-      image: string,
-      episode: Array<string>,
+      name: string;
+      status: string;
+      species: string;
+      gender: string;
+      location: string;
+      image: string;
     }
     this.route.params.subscribe((params) => {
       this.id = params['id'].toString();
@@ -29,16 +25,13 @@ export class CharacterComponent implements OnInit {
     ajax(`https://rickandmortyapi.com/api/character/${this.id}`).subscribe(
       (res: any) => {
         let characterObject: CharacterFull = {
-          id: res.response["id"],
-          name: res.response["name"],
-          status: res.response["status"],
-          species: res.response["species"],
-          gender: res.response["gender"],
-          origin: res.response["origin"],
-          location: res.response["location"],
-          image: res.response["image"],
-          episode: res.response["episode"]
-        }
+          name: res.response['name'],
+          status: res.response['status'],
+          species: res.response['species'],
+          gender: res.response['gender'],
+          location: res.response['location'],
+          image: res.response['image'],
+        };
         this.character.push(characterObject);
       }
     );
--- src/public/characters/character/character.module.ts
+++ src/public/characters/character/character.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CharacterComponent } from './character.component';
+import { HeaderModule } from 'src/shared/components/header/header.module';
 
 @NgModule({
   declarations: [CharacterComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, HeaderModule],
 })
 export class CharacterModule {}
